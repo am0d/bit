8adc03f13368a85f9859f41607b32198119051a8
Tres Walsh <tres.walsh@mnmlstc.com>
2009-10-22 16:51:48 -0700

    Added basic modules, setup script, empty license
    As well as several other empty files. More meat will be added shortly
    
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# buildit install script
+
+from disutils.core import setup
+
+setup(name='buildit',
+      version='0.1',
+	  license='BSD',
+	  description='Minimalistic Build System',
+	  author='Tres Walsh',
+	  author_email='tres.walsh@mnmlstc.com',
+	  url='http://treswalsh.com',
+	  packages=['buildit']
+	 )

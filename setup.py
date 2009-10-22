3613b0c7cd0d605ceaf07e763920634c93b5386b
Tres Walsh <tres.walsh@mnmlstc.com>
2009-10-22 16:56:22 -0700

    Added License (BSD)
     * Fixed spacing in setup.py
    
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from disutils.core import setup
 
 setup(name='buildit',
       version='0.1',
-	  license='BSD',
-	  description='Minimalistic Build System',
-	  author='Tres Walsh',
-	  author_email='tres.walsh@mnmlstc.com',
-	  url='http://treswalsh.com',
-	  packages=['buildit']
+      license='BSD',
+      description='Minimalistic Build System',
+      author='Tres Walsh',
+      author_email='tres.walsh@mnmlstc.com',
+      url='http://treswalsh.com',
+      packages=['buildit']
 	 )

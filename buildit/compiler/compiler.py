7535d14c7ffd266c6efcd6ad7456be43ab994e1b
Tres Walsh <tres.walsh@mnmlstc.com>
2009-10-25 17:26:40 -0700

    Added code to compiler for actually compiling the files :)
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- buildit/compiler/compiler.py
+++ buildit/compiler/compiler.py
@@ -13,7 +13,8 @@ class Compiler(object):
         self.__compile_steps.append(self.setup_files)
         self.__compile_steps.append(self.compile_files)
 
-    def run(self, unity_build):
+    def run(self, file_list, unity_build):
+        self.__file_list = file_list
         for function in self.__compile_steps:
             return_value = function()
             if not return_value == 0:
@@ -21,16 +22,28 @@ class Compiler(object):
         return 0
         
     def setup_files(self):
-        self.file_list = flatten(self.file_list)
-        self.file_list = fix_strings(self.file_list)
-        for file_name in self.file_list:
+        self.__file_list = flatten(self.__file_list)
+        self.__file_list = fix_strings(self.__file_list)
+        for file_name in self.__file_list:
             for extension in self.extensions:
                 if not file_name.endswith(extension):
                     self.file_list.remove(file_name)
         return 0
-        
+
     def compile_files(self):
-        command('{0}: {1}'.format(self.name.upper(), outfile_name))
+        counter = 0
+        for file_name in file_list:
+            out_file = file_name.split('/')
+            out_file = out_file.pop()
+            out_file = '{0}{1}'.format(out_file, output_extension)
+            command('{0}: {1}'.format(self.name.upper(), out_file))
+            try: 
+                return_value = subprocess.call(run_string)
+            except OSError:
+                return_value os.system(run_string)
+            if not return_value == 0:
+                return return_value
+            counter +=1    
         return 0 #; Go away semi-colon, no one loves you!
 
     def add_flags(self, flags):
@@ -39,6 +52,10 @@ class Compiler(object):
     @property
     def exe(self):
         return which('echo')
+        
+    @property
+    def output_extension(self):
+        return '.txt'
         
     @property    
     def extensions(self):

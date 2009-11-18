fac96ee6734c659317a9ac160a827801dd611692
Tres Walsh <tres.walsh@mnmlstc.com>
2009-11-18 02:38:49 -0800

    Moved more of hashdb over to database.
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- buildit/database.py
+++ buildit/database.py
@@ -12,18 +12,23 @@ class Database(object):
         self.__language = None
         self.__location = '.buildit/{0}'.format(self.__project_name)
         self.__connection = sqlite3.connect(self.__location)
+        self.__hash_dictionary = {}
+        # We need to fill the hash_dictionary with values from the hash table.
 
     def add_hash(self, file_name):
-        # More psuedo code than actualy working (so don't use it!)
+        # More psuedo code than actually working (so don't use it!)
         cursor = self.__connection.cursor()
         hash = file_hash(file_name)
         cursor.commit()
         cursor.close()
 
     def get_hash(self, file_name):
-        cursor = self.__connection.cursor()
-        cursor.commit()
-        cursor.close()
+        if file_name in self.__hash_dictionary:
+            return self.__hash_dictionary[file_name]
+
+    def remove_hash(self, file_name):
+        if file_name in self.__hash_dictionary:
+            del self.__hash_dictionary[file_name]
 
     def write(self):
         cursor = self.__connection.cursor()

b4fd6abb3afa71f168d5342ed182871277b74e0e
Tres Walsh <tres.walsh@mnmlstc.com>
2009-11-23 13:24:21 -0800

    Fixed some syntax errors :X
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(name='buildit',
       packages=['buildit',
                 'buildit.system',
                 'buildit.compiler',
-                'buildit.dependency'
+                'buildit.language',
                ]
      )

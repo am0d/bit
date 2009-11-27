5ed9bd19d91251ef8b7dc9e87e6884d5ecf7db75
Tres Walsh <tres.walsh@mnmlstc.com>
2009-11-26 22:17:51 -0800

    Added the ability to compile C files from CXX().
     * Considering moving the Database to the Compiler class. It would allow the changing of compilers to be much easier. It would also be just as easy to update the database name, and everything would stay abstract enough.
    
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- buildit/compiler/cxx.py
+++ buildit/compiler/cxx.py
@@ -8,17 +8,21 @@ class CXX(CC):
 
     def __init__(self):
         CC.__init__(self)
+        self._clang_enabled = False
         self._executable = which('c++')
         self._language = CPP()
 
     @property
     def extensions(self):
-        return ['.cpp', '.cxx', '.c++', '.C']
+        if not self._clang_enabled:
+            return ['.cpp', '.cxx', '.c++', '.C']
+        else:
+            return ['.c', '.cpp', '.cxx', '.c++', '.C']
 
     @property
     def header_files(self):
-        return ['.h', '.hpp']
+        return ['.h', '.hpp', '.hxx', '.h++']
 
-    @property # Requires a new implementation 
+    @property 
     def enable_c(self):
-        pass
+        self._clang_enabled = True

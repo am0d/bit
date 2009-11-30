8b8fbc13f142688aa72c029ef3671e10a08f5628
a_m0d <damien.schoof@gmail.com>
2009-11-30 19:20:46 +0800

    Fix to make the globbing of files actually work - compiler's extensions already begin with '.', so no need to include it in the glob() input string as well

--- buildit/system/system.py
+++ buildit/system/system.py
@@ -67,12 +67,12 @@ class System(threading.Thread):
                         if recurse:
                             for root, dir, files in os.walk(item):
                                 for extension in self.compiler.extensions:
-                                    glob_list += glob('{0}/*.{1}'.format(root,
-                                        extension))
+                                    glob_list.append(glob('{0}/*{1}'.format(root,
+                                        extension)))
                         else:
                             for extension in self.compiler.extensions:
-                                glob_list = glob('{0}/*.{1}'.format(item, 
-                                    extension))
+                                glob_list.append(glob('{0}/*{1}'.format(item, 
+                                    extension)))
                         for file_name in glob_list:
                             if os.path.isfile(file_name):
                                 self._file_list.append(file_name)
@@ -83,12 +83,13 @@ class System(threading.Thread):
                 glob_list = []
                 if recurse:
                     for root, dir, file_names in os.walk(files):
+                        print root + ' ' + dir + ' ' + files
                         for extension in self.compiler.extensions:
-                            glob_list += glob('{0}/*.{1}'.format(root, 
-                                extension))
+                            glob_list.append(glob('{0}/*{1}'.format(root, 
+                                extension)))
                 else:
                     for extension in self.compiler.extensions:
-                        glob_list = glob('{0}/*.{1}'.format(files, extension))
+                        glob_list = glob('{0}/*{1}'.format(files, extension))
                 for file_name in glob_list:
                     if os.path.isfile(file_name):
                         self._file_list.append(file_name)

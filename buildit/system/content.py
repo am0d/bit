349f3464f028cab0b8c20e61671e7f9020081c2e
Tres Walsh <tres.walsh@mnmlstc.com>
2009-12-05 19:11:26 -0800

    OK I TESTED IT THIS IS FOR REAL I SWEAR IT WORKS.
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- buildit/system/content.py
+++ buildit/system/content.py
@@ -1,10 +1,10 @@
 # Used for files, and other non compiled stuff
 
 from buildit.system.system import System 
-from buildit.compiler.content import Content
+from buildit.compiler.content import Content as ContentC
 
 class Content(System):
 
     def __init__(self, project_name):
         System.__init__(self, project_name)
-        self.compiler = Content(project_name)
+        self.compiler = ContentC(project_name)

22f4485a419c47311bd425ab6a24052b49f751a3
Tres Walsh <tres.walsh@mnmlstc.com>
2009-12-31 01:45:55 -0800

    Added a basic stylesheet which will be added to for the documentation
     * Added more to the doc build script. Shouldn't take too long to finish
       it
     * Added html folder + html files to .gitignore, as they are generated,
       and we really won't have a use for them :/
    
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- doc/build.py
+++ doc/build.py
@@ -3,6 +3,34 @@
 
 import os
 import sys
+import shutil
 
 import markdown2
 
+def generate_docfile(file_name):
+    return file_name
+
+def fix_paths(file_list):
+    if sys.platform == 'win32':
+        file_list = [file_name.replace('\\', '/') for file_name in file_list]
+        file_list.sort()
+    return file_list
+
+if __name__ == '__main__':
+    try:
+        os.makedirs('../html')
+    except OSError:
+        pass
+    file_list = []
+    for root, dir, files in os.walk('.'):
+        for file in files:
+            file_list.append('{0}/{1}'.format(root, file))
+    file_list = fix_paths(file_list)
+    for file in file_list:
+        if file.endswith('.txt'):
+            file = generate_docfile(file)
+        try:
+            shutil.copyfile(file, '../html')
+        except:
+            pass
+            

df80efef84d73de34ee0bb7d40df87b1a5d8fa87
Tres Walsh <tres.walsh@mnmlstc.com>
2010-01-13 01:11:44 -0800

    Beginning the real work on the MSVC System
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- buildit/system/msvc.py
+++ buildit/system/msvc.py
@@ -1,10 +1,39 @@
 # Microsoft Visual C Compiler
 
-from buildit.system.unix import Unix
+from buildit.system.system import System
+from buildit.compiler.msvc import MSVC as MSVCompiler
 from buildit.utils import which
 from buildit.cprint import error
 
 class MSVC(System):
 
     def __init__(self, project_name):
-        self.compiler.executable = 'cl'
+        System.__init__(self, project_name)
+        self.compiler = MSVCompiler()
+        self.compiler_version = 2008 # default version for now :)
+        self._build_steps.insert(setup_environment, 0)
+
+    # Sets up our system path for MSVC (so others don't have to)
+    def setup_environment(self):
+        
+        return 0
+
+    @property
+    def vs6(self): #Are you crazy?!
+        self.compiler_version = 6
+
+    @property
+    def vs2003(self):
+        self.compiler_version = 2003
+
+    @property
+    def vs2005(self):
+        self.compiler_version = 2005
+
+    @property
+    def vs2008(self):
+        self.compiler_version = 2008
+
+    @property
+    def vs2010(self):
+        self.compiler_version = 2010

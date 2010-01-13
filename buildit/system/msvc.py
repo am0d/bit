abb0497f01560a2451b0c76e3d81640c2588c02b
Tres Walsh <tres.walsh@mnmlstc.com>
2010-01-13 01:24:13 -0800

    Just a few more things.
     * Will have to do some serious work to get this working right. Hooray
       for unstable stuffffff :/
    
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- buildit/system/msvc.py
+++ buildit/system/msvc.py
@@ -10,30 +10,34 @@ class MSVC(System):
     def __init__(self, project_name):
         System.__init__(self, project_name)
         self.compiler = MSVCompiler()
-        self.compiler_version = 2008 # default version for now :)
+        self.__compiler_version = 2008 # Used only to help figure out the install path
+        self.__arch = 32 # Everyone uses 32 bit right? RIGHT?
         self._build_steps.insert(setup_environment, 0)
 
     # Sets up our system path for MSVC (so others don't have to)
     def setup_environment(self):
-        
         return 0
 
     @property
+    def x64(self):
+        self.__arch = 64
+
+    @property
     def vs6(self): #Are you crazy?!
-        self.compiler_version = 6
+        self.__compiler_version = 6
 
     @property
     def vs2003(self):
-        self.compiler_version = 2003
+        self.__compiler_version = 2003
 
     @property
     def vs2005(self):
-        self.compiler_version = 2005
+        self.__compiler_version = 2005
 
     @property
     def vs2008(self):
-        self.compiler_version = 2008
+        self.__compiler_version = 2008
 
     @property
     def vs2010(self):
-        self.compiler_version = 2010
+        self.__compiler_version = 2010

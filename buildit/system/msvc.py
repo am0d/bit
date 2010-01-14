a810bc8f15a65688fa41fea4fbb39c2cefb47eed
Tres Walsh <tres.walsh@mnmlstc.com>
2010-01-13 17:52:05 -0800

    Found the quickest and easiest way to find the installed Visual Studio version
     * Still doesn't do anything, work may be slow due to life :/
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- buildit/system/msvc.py
+++ buildit/system/msvc.py
@@ -10,12 +10,13 @@ class MSVC(System):
     def __init__(self, project_name):
         System.__init__(self, project_name)
         self.compiler = MSVCompiler()
-        self.__compiler_version = 2008 # Used only to help figure out the install path
+        self.__compiler_version = 'VS90COMNTOOLS' # Used only to help figure out the install path
         self.__arch = 32 # Everyone uses 32 bit right? RIGHT?
         self._build_steps.insert(setup_environment, 0)
 
     # Sets up our system path for MSVC (so others don't have to)
     def setup_environment(self):
+        print os.environ[self.__compiler_version]
         return 0
 
     @property
@@ -24,20 +25,20 @@ class MSVC(System):
 
     @property
     def vs6(self): #Are you crazy?!
-        self.__compiler_version = 6
+        self.__compiler_version = 'VS90COMNTOOLS' # Doesn't work yet!
 
     @property
     def vs2003(self):
-        self.__compiler_version = 2003
+        self.__compiler_version = 'VS71COMNTOOLS'
 
     @property
     def vs2005(self):
-        self.__compiler_version = 2005
+        self.__compiler_version = 'VS80COMNTOOLS'
 
     @property
     def vs2008(self):
-        self.__compiler_version = 2008
+        self.__compiler_version = 'VS90COMNTOOLS'
 
     @property
     def vs2010(self):
-        self.__compiler_version = 2010
+        self.__compiler_version = 'VS100COMNTOOLS' # Not considered accurate right now XD

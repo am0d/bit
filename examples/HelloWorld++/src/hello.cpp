ce05fdd1bdf284adf18e4dd909907b7cb2ff3900
Tres Walsh <tres.walsh@mnmlstc.com>
2010-01-15 17:29:23 -0800

    Removed the very old, and very broken examples folder.
     * It will return before 0.2 is officially released, but until that
       time, it currently serves no purpose.
    
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- examples/HelloWorld++/src/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-#include <iostream>
-
-int main(int argc, char* argv[])
-{
-	std::vector<std::string> parameters(argv, argv + argc);
-	std::cout << "Hello, World!" << std::endl;
-}

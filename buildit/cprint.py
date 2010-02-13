4e3b829c50156e6c667b66e6237eeaaa6602548e
Tres Walsh <tres.walsh@mnmlstc.com>
2010-02-13 11:49:22 -0800

    Fixed Issue #6
     * Colors should work properly on Windows now. :)
    
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- buildit/cprint.py
+++ buildit/cprint.py
@@ -9,6 +9,32 @@ if sys.platform == 'win32':
     green = 0x0002
     red = 0x0004
     
+    handle = ctypes.windll.kernel32.GetStdHandle(-11)
+    short = ctypes.c_short
+    ushort = ctypes.c_ushort
+    
+    # Well it's not *all* bad :V
+    class Coord(ctypes.Structure):
+        _fields_ =[('X', short), ('Y', short)]
+    class SmallRect(ctypes.Structure):
+        _fields_ = [('Left', short), ('Top', short), 
+                    ('Right', short), ('Bottom', short)]
+    class ConsoleScreenBufferInfo(ctypes.Structure):
+        _fields_ = [('dwSize', Coord), ('dwCursorPosition', Coord),
+                    ('wAttributes', ushort), ('srWindow', SmallRect), 
+                    ('dwMaximumWindowSize', Coord)]
+
+    csbi = ConsoleScreenBufferInfo()
+    # Keep in line with the Windows API. Sort of >_>
+    def get_console_text_attribute(handle):
+        ctypes.windll.kernel32.GetConsoleScreenBufferInfo(handle, 
+            ctypes.byref(csbi))
+        return csbi.wAttributes
+
+    default_colors = get_console_text_attribute(handle)
+    background = default_colors & 0x0070
+    primary_color = default_colors & 0x0007
+
 else:
     magenta = '\33[1;35m'
     yellow = '\33[1;33m'
@@ -21,9 +47,11 @@ def cprint(message, color):
     print_lock.acquire()
     if sys.platform == 'win32':
         handle = ctypes.windll.kernel32.GetStdHandle(-11)
-        ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color | 0x0008)
+        ctypes.windll.kernel32.SetConsoleTextAttribute(handle, 
+            color | 0x0008 | background)
         print(message),
-        ctypes.windll.kernel32.SetConsoleTextAttribute(handle, 0x0007)
+        ctypes.windll.kernel32.SetConsoleTextAttribute(handle, 
+            primary_color | 0x0008 | background)
         print('')
     else:
         print('{0}{1}\33[0m'.format(color, message))

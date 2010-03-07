7fde3134b583fb42f83a7b5399d1cb61a9f7c642
Tres Walsh <tres.walsh@mnmlstc.com>
2010-03-06 17:57:21 -0800

    Created new basic implementation of cprint.
     * Need to look into stopping certain colors from being printed (i.e.
       white on white, black on black etc.) The API is much neater this time around.
    
     * cprint has been renamed to color_print
    
     * In addition, there is now a char_print function which will be used
       for the eventual progress bar.
    
    Might need to implement a singleton class for the GPL, to allow plguins
    to import from the buildit module. :)
    
    Signed-off-by: Tres Walsh <tres.walsh@mnmlstc.com>

--- buildit/cprint.py
+++ buildit/cprint.py
@@ -1,70 +1,92 @@
-# Color printing messages 
+# Color Printing Module
 
 import sys
-import threading
+import multiprocessing
+
 if sys.platform == 'win32':
     import ctypes
-    magenta = 0x0005
-    yellow = 0x0006
-    green = 0x0002
-    red = 0x0004
-    
+    fcolor = { 'blue'   : 0x0001,
+               'green'  : 0x0002,
+               'cyan'   : 0x0003,
+               'red'    : 0x0004,
+               'magenta': 0x0005, 
+               'yellow' : 0x0006,
+               'white'  : 0x0007,
+             }
+
     handle = ctypes.windll.kernel32.GetStdHandle(-11)
     short = ctypes.c_short
     ushort = ctypes.c_ushort
-    
-    # Well it's not *all* bad :V
+
     class Coord(ctypes.Structure):
-        _fields_ =[('X', short), ('Y', short)]
+        _fields_ = [('X', short), ('Y', short)]
+
     class SmallRect(ctypes.Structure):
-        _fields_ = [('Left', short), ('Top', short), 
+        _fields_ = [('Left', short), ('Top', short),
                     ('Right', short), ('Bottom', short)]
+
     class ConsoleScreenBufferInfo(ctypes.Structure):
         _fields_ = [('dwSize', Coord), ('dwCursorPosition', Coord),
-                    ('wAttributes', ushort), ('srWindow', SmallRect), 
+                    ('wAttributes', ushort), ('srWindow', SmallRect),
                     ('dwMaximumWindowSize', Coord)]
 
+        @property
+        def attributes(self):     
+            return self.wAttributes
+
     csbi = ConsoleScreenBufferInfo()
-    # Keep in line with the Windows API. Sort of >_>
+
     def get_console_text_attribute(handle):
-        ctypes.windll.kernel32.GetConsoleScreenBufferInfo(handle, 
-            ctypes.byref(csbi))
-        return csbi.wAttributes
+        ctypes.windll.kernel32.GetConsoleScreenBufferInfo(handle,
+                ctypes.byref(csbi))
+        return csbi.attributes
+    set_console_text_attribute = ctypes.windll.kernel32.SetConsoleTextAttribute
 
     default_colors = get_console_text_attribute(handle)
     background = default_colors & 0x0070
     primary_color = default_colors & 0x0007
 
-else:
-    magenta = '\33[1;35m'
-    yellow = '\33[1;33m'
-    green = '\33[1;32m'
-    red = '\33[1;31m'
-    
-print_lock = threading.Lock()
+else: # Assume the terminal supports ascii colors
+    fcolor = { 'red'    : '\33[1;31m',
+               'green'  : '\33[1;32m',
+               'yellow' : '\33[1;33m',
+               'blue'   : '\33[1;34m',
+               'magenta': '\33[1;35m',
+               'cyan'   : '\33[1;36m',
+               'white'  : '\33[1;37m',
+             }
 
-def cprint(message, color):
+print_lock = multiprocessing.Lock()
+
+def color_print(message, color):
     print_lock.acquire()
     if sys.platform == 'win32':
-        handle = ctypes.windll.kernel32.GetStdHandle(-11)
-        ctypes.windll.kernel32.SetConsoleTextAttribute(handle, 
-            color | 0x0008 | background)
+        set_console_text_attribute(handle, fcolor[color] | 0x0008 | background)
         print(message),
-        ctypes.windll.kernel32.SetConsoleTextAttribute(handle, 
-            primary_color | 0x0008 | background)
+        set_console_text_attribute(handle, primary_color | 0x0008 | background)
         print('')
     else:
-        print('{0}{1}\33[0m'.format(color, message))
+        print('{0}{1}\33[0m'.format(fcolor[color], message))
+    print_lock.release()
+
+def char_print(char, color):
+    print_lock.acquire()
+    if sys.platform == 'win32':
+        set_console_text_attribute(handle, fcolor[color] | 0x0008 | background)
+        sys.stdout.write(char)
+        set_console_text_attribute(handle, primary_color | 0x0008 | background)
+    else:
+        print('{0}{1}\33[0m'.format(fcolor[color], char))
     print_lock.release()
 
 def info(message):
-    cprint(message, green)
-    
+    color_print(message, 'green')
+
 def warning(message):
-    cprint(message, yellow)
+    color_print(message, 'yellow')
 
 def error(message):
-    cprint(message, red)
-    
+    color_print(message, 'red')
+
 def command(message):
-    cprint(message, magenta)
+    color_print(message, 'magenta')

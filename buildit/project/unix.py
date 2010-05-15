5caa464f2ef868f423c16c3f65f031624321a9af
Tres Walsh <tres.walsh@mnmlstc.com>
2010-05-15 01:45:53 -0700

    the "Unix" Project type is effectively finished.
     * Will being making sure GCC works with it ;)

--- buildit/project/unix.py
+++ buildit/project/unix.py
@@ -1,7 +1,12 @@
 # Basic Unix Program
 
+import os
+import subprocess
+
 from buildit.project.project import Project
 from buildit.compiler.gcc import GCC
+from buildit.utils import flatten
+from buildit.cprint
 
 class Unix(Project):
 
@@ -11,3 +16,57 @@ class Unix(Project):
 
     def __str__(self):
         return 'Unix'
+
+    def add_pkg(self, *scripts):
+        for script in flatten(list(set(scripts))):
+            cflags = config_script_flags(script, '', '--cflags')
+            lflags = config_script_flags(script, '', '--libs')
+            self.compiler.add_compile_flags(cflags)
+            self.compiler.add_link_flags(cflags, lflags)
+
+    def pkg_config(self, (packages):
+        for package in flatten(list(set(packages))):
+            cflags = config_script_flags('pkg', package, '--cflags')
+            lflags = config_script_flags('pkg', package, '--libs')
+            self.compiler.add_compile_flags(cflags)
+            self.compiler.add_link_flags(cflags, lflags)
+
+    def add_define(self, *defines):
+        self.compiler.add_define(defines)
+
+    def add_include_directory(self, *directories):
+        self.compiler.add_include_directory(directories)
+
+    def add_library_directory(self, *directories):
+        self.compiler.add_library_directory(directories)
+
+    def add_library(self, *libraries):
+        self.compiler.add_library(libraries)
+
+    def add_flag(self, *flags):
+        self.compiler.add_flags(flags)
+
+    def add_link_flag(self, *flags):
+        self.compiler.add_link_flags(flags)
+
+    @property
+    def C99(self):
+        self.compiler.C99
+
+    @property
+    def CXX(self):
+        self.compiler.CXX
+
+    @property
+    def CPP(self):
+        self.compiler.CXX
+
+def config_script_flags(script, package, argument):
+    process = subprocess.Popen(['{0}-config'.format(script), package, argument], 
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               universal_newlines=True)
+    output, errput = process.communicate()
+    if not output:
+        error('{0}: {1}'.format(package, errput))
+        return
+    return output.replace('\n', '')

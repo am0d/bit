0310d865983a352722341520b80ff6503bbec7fc
Tres Walsh <tres.walsh@mnmlstc.com>
2010-06-01 20:54:02 -0700

    I don't think I thought this out thoroughly. Must be the fever.

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ if not sys.platform == 'win32':
     script_list.remove('scripts/bit.bat')
 if sys.platform == 'win32':
     os.rename('scripts/bit', 'scripts/bit_script.py')
+    script_list.remove('scripts/bit')
+    script_list.append('scripts/bit_script.py')
 setup(name='bit',
       version='0.3',
       license='BSD',

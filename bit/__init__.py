6d34abc0a0416274db8bf1da9f5a30a69589c6d2
Tres Walsh <tres.walsh@mnmlstc.com>
2010-07-13 20:24:26 -0700

    Added a boolean variable for BSD systems.

--- bit/__init__.py
+++ bit/__init__.py
@@ -10,6 +10,7 @@ from bit.project.content import Content
 # Compiler Modules
 from bit.compiler.cc import CC
 from bit.compiler.clang import Clang
+from bit.compiler.msvc import MSVCCompiler
 
 # Global Project Lookup
 from bit.gpl import GPL
@@ -20,6 +21,7 @@ import sys
 windows = False
 macosx = False
 linux = False
+bsd = False
 
 if sys.platform == 'win32':
     windows = True
@@ -27,6 +29,7 @@ if sys.platform == 'darwin':
     macosx = True
 if sys.platform == 'linux2':
     linux = True
-
+if 'bsd' in sys.platform:
+    bsd = True
 # Assuming Direct Control
 bit = GPL()
